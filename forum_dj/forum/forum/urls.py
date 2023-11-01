a9a458287a5c863ff79c59ca3dc7fbcb51785518
Vitalik <vitalikfedytnyk@gmail.com>
2023-11-01 23:19:32 +0200

    final_login

--- forum_dj/forum/forum/urls.py
+++ forum_dj/forum/forum/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('forum_members/', include('forum_members.urls'))
     ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
